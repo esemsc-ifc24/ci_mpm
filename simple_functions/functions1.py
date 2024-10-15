afbba4df2a707faa3e358172e6d21ac4e3b29f64
esemsc-ifc24 <ida.caspary24@imperial.ac.uk>
2024-10-15 11:21:46 +0100

    Updated factorial function and tests

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,7 @@
+__all__ = ['my_sum', 'factorial']
 
-__all__ = ['my_sum']
+from functools import cache
+import pytest
 
 
 def my_sum(iterable):
@@ -7,3 +9,19 @@ def my_sum(iterable):
     for i in iterable:
         tot += i
     return tot
+
+
+@cache
+def factorial(n):
+    return n * factorial(n - 1) if n else 1
+
+
+@pytest.mark.parametrize('number, expected', [
+        (5, 120),
+        (3, 6),
+        (1, 1)
+    ])
+def test_factorial(number, expected):
+    '''Test our factorial function'''
+    answer = factorial(number)
+    assert answer == expected
